14fdd7c5b1b4125593b3a710ece2b27617d2738d
Kevin <Ausadmin@users.noreply.huggingface.co>
2025-11-01 04:37:51 +0000

    Create app.py

---
+++ app.py
@@ -0,0 +1,11 @@
+import sys
+import os
+
+# ensure it runs from the right folder
+sys.path.append(os.path.join(os.path.dirname(__file__), "app"))
+
+# run your real main
+from app.main import main
+
+if __name__ == "__main__":
+    main()
