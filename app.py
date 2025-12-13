0a00ade162cf87042f20ffd423b601d84f4cb4ae
Kevin <Ausadmin@users.noreply.huggingface.co>
2025-12-13 04:27:57 +0000

    Update app.py

--- app.py
+++ app.py
@@ -4,8 +4,10 @@ import os
 # ensure it runs from the right folder
 sys.path.append(os.path.join(os.path.dirname(__file__), "app"))
 
-# run your real main
-from app.main import main
+from app.server import app
 
-if __name__ == "__main__":
-    main()
+# run your real main
+# from app.main import main
+
+#if __name__ == "__main__":
+    # main()
