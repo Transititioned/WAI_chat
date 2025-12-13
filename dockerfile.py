90db874e7873447618e9d8871588471e88317349
Kevin <Ausadmin@users.noreply.huggingface.co>
2025-12-13 05:35:07 +0000

    Create dockerfile.py

---
+++ dockerfile.py
@@ -0,0 +1,13 @@
+FROM python:3.10-slim
+
+WORKDIR /app
+COPY . /app
+
+# Optional but helps some builds
+RUN pip install --no-cache-dir -U pip
+
+RUN pip install --no-cache-dir -r requirements.txt
+
+EXPOSE 7860
+
+CMD ["uvicorn", "app:app", "--host", "0.0.0.0", "--port", "7860"]
